70addd877691a4f824d1cf821f23ea703121ca50
Arshjot <arshjots11@gmail.com>
2022-01-13 16:32:23 -0600

    Add some transitions

--- main.js
+++ main.js
@@ -79,11 +79,13 @@ rotating.addEventListener('wheel', e => {
 });
 
 var menuBtn = document.querySelector('.menu-btn');
+
 var nav = document.querySelector('.nav-2');
 var lineOne = document.querySelector('.nav-2 .menu-btn .line--1');
 var lineTwo = document.querySelector('.nav-2 .menu-btn .line--2');
 var lineThree = document.querySelector('.nav-2 .menu-btn .line--3');
 var link = document.querySelector('.nav-2 .nav-links');
+
 menuBtn.addEventListener('click', () => {
     nav.classList.toggle('nav-open');
     lineOne.classList.toggle('line-cross');
